db3580b716c6d1802d192d834c830e1d8a5e485f
Berkay <berkaygumus32@gmail.com>
2022-02-21 23:12:52 -0700

    index.html bugs fixed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Get method
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/index.html");
     
 });
 
